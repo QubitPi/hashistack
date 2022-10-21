030860b5183825fcd65e46a5e6303e8b73a5b58d
Jack <jack20191124@proton.me>
2022-10-21 14:35:58 +0800

    Add script for installing Certbot

--- install-certbot.sh
+++ install-certbot.sh
@@ -14,25 +14,14 @@
 #!/bin/bash
 
 : <<'END'
-Install Jenkins Long Term Support Release version on Ubuntu VM
+Install Certbot for SSL through Nginx.
+
+To start certificate signing process, run
+
+  sudo certbot --nginx
+
 END
 
-# Install Java
-sudo apt update
-sudo apt install openjdk-11-jre -y
-
-# Install Jenkins
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
-/usr/share/keyrings/jenkins-keyring.asc > /dev/null
-
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-/etc/apt/sources.list.d/jenkins.list > /dev/null
-
-sudo apt-get update
-sudo apt-get install jenkins -y
-
-# Prepare for HTTPS certificate
 sudo apt install snapd -y
 sudo snap install core; sudo snap refresh core
 sudo snap install --classic certbot
