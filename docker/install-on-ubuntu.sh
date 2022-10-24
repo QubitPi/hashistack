d3b1f1c23f742660920b4ea92372a91d31b69ff3
Jack <jack20191124@proton.me>
2022-10-24 17:42:41 +0800

    Auto confirming add-apt-repository
    https://askubuntu.com/a/651045/1127171

--- docker/install-on-ubuntu.sh
+++ docker/install-on-ubuntu.sh
@@ -48,7 +48,7 @@ sudo apt install apt-transport-https ca-certificates curl software-properties-co
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 # Add the Docker repository to APT sources
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+yes '' | sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 
 # Make sure we are about to install from the Docker repo instead of the default Ubuntu repo:
 apt-cache policy docker-ce
