3d4689b3f01401f42e77e699918eb4852f91a838
Jack <jack20220723@gmail.com>
2023-10-28 21:52:10 +0800

    Support deploying MLflow model container onto AWS through HashiCorp

---
+++ hashicorp/mlflow/scripts/aws-mlflow-docker-pkr-setup.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+set -x
+set -e
+
+# Copyright Jiaqi Liu
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+sudo apt update
+sudo apt upgrade -y
+
+# Set up Python
+sudo apt-get -y install python3-pip
+pip3 install --upgrade pip
+sudo apt -y install python3.10-venv
+PATH=$PATH:/home/ubuntu/.local/bin
+
+python3 --version
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# Install Docker
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo usermod -aG docker $USER
+sudo chmod o+rw /var/run/docker.sock # https://stackoverflow.com/a/76329637
+
+# Build ML model Docker image
+pip install mlflow
+pip install mlflow-skinny
+pip install mlflow[extras]
+mlflow models build-docker --name "mlflow-model-container"
---
+++ hashicorp/mlflow/scripts/aws-mlflow-docker-tf-init.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -x
+set -e
+
+# Copyright Jiaqi Liu
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+touch /home/ubuntu/aws-mlflow-docker-tf-init.log
+export TF_INIT_LOG=/home/ubuntu/aws-mlflow-docker-tf-init.log
+echo "aws-base-tf-init started executing..."       >>$TF_INIT_LOG 2>&1
+
+for ML_MODEL_PATH in /home/ubuntu/ml-models/*; do
+  PUBLIC_PORT=$( cat $ML_MODEL_PATH/PORT )                                           >>$TF_INIT_LOG 2>&1
+  echo "Running container serving model under $ML_MODEL_PATH with port $PUBLIC_PORT" >>$TF_INIT_LOG 2>&1
+
+  sudo docker run --detach --rm \
+    --memory=4000m \
+    -p $PUBLIC_PORT:8080 \
+    -e PYTHONPATH="/opt/ml/model:$PYTHONPATH" \
+    -v $ML_MODEL_PATH:/opt/ml/model \
+    -e GUNICORN_CMD_ARGS="--timeout 600 -k gevent --workers=1" \
+    "mlflow-model-container"                                                         >>$TF_INIT_LOG 2>&1
+
+  echo "Model container started"                                                     >>$TF_INIT_LOG 2>&1
+done
+
+echo "aws-base-tf-init finished executing"                                           >>$TF_INIT_LOG 2>&1
