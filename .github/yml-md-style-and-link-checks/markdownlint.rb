b9eb6e1668b16b2cd49bf1b55b62d1fa302b938e
Jiaqi Liu <jack20220723@gmail.com>
2024-04-20 11:01:54 +0800

    Add semantic version bump action (#193)

--- .github/yml-md-style-and-link-checks/markdownlint.rb
+++ .github/yml-md-style-and-link-checks/markdownlint.rb
@@ -13,7 +13,6 @@
 # limitations under the License.
 
 all
-# https://github.com/markdownlint/markdownlint/blob/main/docs/RULES.md
 rule 'MD003', style: :setext_with_atx
 rule 'MD004', style: :sublist
 rule 'MD013', line_length: 120
