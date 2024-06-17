366e71125b019065f26bc92e655bc3698f6d4d81
Hutao of Emberfire <jack20220723@gmail.com>
2024-06-18 07:49:54 +0800

    Revert 212
    This reverts commit 3b00061d2c0f2da03f27d860ce2656ecc8d444b9.

--- hashicorp/machine-learning/scripts/mlflow-docker/aws-ml-pkr-setup.sh
+++ hashicorp/machine-learning/scripts/mlflow-docker/aws-ml-pkr-setup.sh
@@ -20,19 +20,10 @@ sudo apt update
 sudo apt upgrade -y
 
 # Set up Python
-#sudo apt-get -y install python3-pip
-#pip3 install --upgrade pip
-#sudo apt -y install python3.10-venv
-#PATH=$PATH:${HOME_DIR}/.local/bin
-sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev -y
-wget https://www.python.org/ftp/python/3.8.0/Python-3.8.0.tgz
-tar -xf Python-3.8.0.tgz
-cd Python-3.8.0
-./configure --enable-optimizations
-make -j 8
-sudo make altinstall
-alias python=python3.8
-sudo apt-get install python3-pip -y
+sudo apt-get -y install python3-pip
+pip3 install --upgrade pip
+sudo apt -y install python3.10-venv
+PATH=$PATH:${HOME_DIR}/.local/bin
 
 python3 --version
 
